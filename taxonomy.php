9f0fef38f72ea34f3f40af857e2ee1658226b54f
yogarasu <ishiguro@revoltage.jp>
2023-01-04 12:55:26 +0900

    fix #31 構文を修正

--- taxonomy.php
+++ taxonomy.php
@@ -2,5 +2,5 @@
 $taxonomy = get_query_var('taxonomy');
 $post_type = get_taxonomy($taxonomy)->object_type[0];
 if (is_archive($post_type)) {
-  include_once("archives/${post_type}.php");
+  include_once("archives/{$post_type}.php");
 }
