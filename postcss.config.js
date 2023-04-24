e1b13a4781ef5fd0d8023f871b790b7387e6ada7
yogarasu <ishiguro@revoltage.jp>
2023-04-24 15:54:25 +0900

    fix #152 postcss-inline-svgを削除

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    // BUG: Loading PostCSS Plugin failed
-    // https://github.com/TrySound/postcss-inline-svg/issues/104
-    // 'postcss-inline-svg': {},
     'postcss-preset-env': {
       stage: 3,
       features: {
