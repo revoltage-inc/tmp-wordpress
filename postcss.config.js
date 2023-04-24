3716f26a3e96601e713bd14daabeaafe3a0fe7a5
yogarasu <ishiguro@revoltage.jp>
2023-04-24 15:58:24 +0900

    fix #152 CSSの圧縮ライブラリをpostcss-cssoからcssnanoへ変更

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-preset-env': { stage: 3, features: { 'nesting-rules': true } },
-    'postcss-csso': { restructure: false },
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: { preset: 'default' },
   },
 }
