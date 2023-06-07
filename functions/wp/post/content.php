8f1acc754ab76808b376de00ff09f04fa37a2c70
yogarasu <ishiguro@revoltage.jp>
2023-06-08 06:09:01 +0900

    fix #180 自動pタグ無効化設定を有効化

--- functions/wp/post/content.php
+++ functions/wp/post/content.php
@@ -5,7 +5,7 @@
  */
 
 // Disable automatic insert of p-tag and br-tag
-// remove_filter('the_content', 'wpautop');
+remove_filter('the_content', 'wpautop');
 
 // Disable automatic convert of symbols, etc
 remove_filter('the_content', 'wptexturize');
